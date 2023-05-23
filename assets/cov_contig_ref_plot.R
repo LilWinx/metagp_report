5ef750d4de98407b1bec4b74009f80302bd71f12
LilWinx <lil.winx@gmail.com>
2023-05-23 13:40:10 +1000

    add args

--- assets/cov_contig_ref_plot.R
+++ assets/cov_contig_ref_plot.R
@@ -19,8 +19,12 @@ library(grid)
 # written by Carl Suster @arcesu
 
 # input data
+#filter_coords <- args[1]
+#cov <- args[2]
+#outname <- args[3]
 filter_coords <- read_tsv("3160270484_S3.NC_002929_filter_coords.txt", skip = 3)
 cov <- read_tsv("3160270484_S3_NC_002929.2.depth.txt")
+outname <- "3160270484_S3_NC_002929.2_coverageplot.png"
 
 # clean up contigs file to collapse overlapping regions
 endpoint <- filter_coords %>%
@@ -48,7 +52,7 @@ p1 <-
   binned_cov %>%
   #cov %>% # switch to this for no bin!
   ggplot(aes(x=Position, y=Coverage)) +
-  geom_col(fill="skyblue") +
+  geom_col(fill="#5D3A9B") +
   labs(
     title = "Test",
     x = NULL,
@@ -60,12 +64,12 @@ p1 <-
   ) +
   scale_x_continuous(labels = scales::comma, limits = c(1, max(cov$Position, merged_df$end))) +
   scale_y_continuous(trans='log10') +
-  theme_bw()
+  theme_classic()
 
 # draw contigs plot
 p2 <- 
   ggplot(merged_df) +
-  geom_segment(aes(x = start, xend = end, y = 0, yend = 0), colour = "red", linewidth = 10) +
+  geom_segment(aes(x = start, xend = end, y = 0, yend = 0), colour = "#E66100", linewidth = 10) +
   annotation_custom(grid::textGrob("Contigs", x = grid::unit(0, "mm"), hjust = 0, gp = grid::gpar(fontsize = 8))) +
   labs(
     x = "Base position in genome",
@@ -79,7 +83,7 @@ p2 <-
 p1 / p2 + plot_layout(ncol = 1, heights = c(10, 1))
 
 # save!
-ggsave("cov_contig.png",
+ggsave(outname,
        width = 1500,
        height = 500,
        units = "px",
