8cc7b835cf68a042b6a401c5b3a151948d7d87ec
LilWinx <lil.winx@gmail.com>
2023-07-03 17:01:17 +1000

    change to ncbi_db datasets

--- py_scripts/tpmAbundances_top10.py
+++ py_scripts/tpmAbundances_top10.py
@@ -18,36 +18,16 @@ def read_in_tpm(tpm_file):
                 "Genus",
                 "Species",
     ]
-    
-    fungi_phyla = [
-        "Ascomycota",
-        "Basidiomycota",
-        "Chytridiomycota",
-        "Microsporidia",
-        "Glomeromycota",
-        "Zygomycota"
-    ]
-
-    parasite_phyla = [
-        "Apicomplexa",
-        "Ciliophora",
-        "Bacillariophyta",
-        "Cercozoa",
-        "Euglenozoa",
-        "Heterolobosea",
-        "Parabasalia",
-        "Fornicata",
-        "Evosea",
-    ]
-
+    fungi_db = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(__file__)), "database/fungi.txt"), sep = "\t", header = 0)
+    parasites_db = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(__file__)), "database/parasites.txt"), sep = "\t" , header = 0)
     input_file = pd.read_csv(tpm_file, sep="\t", names=colnames)
     tpm_input = input_file[input_file["Phylum"].str.contains("Chordata")==False]
     tpm_file = tpm_input[:50].reset_index(drop=True)
     d_bacteria = tpm_file[
         tpm_file['Kingdom'].str.contains("Bacteria")][:10].reset_index(drop=True)
     d_viruses = tpm_file[tpm_file['Kingdom'].str.contains("Viruses")][:10].reset_index(drop=True)
-    k_fungi = tpm_file[tpm_file['Phylum'].str.contains('|'.join(fungi_phyla))][:10].reset_index(drop=True)
-    k_parasite = tpm_file[tpm_file['Phylum'].str.contains('|'.join(parasite_phyla))][:10].reset_index(drop=True)
+    k_fungi = tpm_file[tpm_file['Species'].isin(fungi_db['#Organism/Name'])]
+    k_parasite = tpm_file[tpm_file['Species'].isin(parasites_db['#Organism/Name'])]
     for tpm_df in [d_bacteria, d_viruses, k_fungi, k_parasite]:
         tpm_df.sort_values(by="TPM", ascending = False)
     bacteria_list = d_bacteria['Species'].to_list()
@@ -61,8 +41,9 @@ def read_in_tpm(tpm_file):
         (fungi_list, "Fungi"), 
         (parasite_list, "Parasites")
     ]
-
+    
     for species_list, dk_status in kd_list:
         accordion_stuff = pathogen_db_search.pathogen_search(species_list, dk_status)
         accordion_dict.update(accordion_stuff)
     return accordion_dict
+    
