e8e8e4be187c3f44c9f13827dbdfae4f570149ca
LilWinx <lil.winx@gmail.com>
2023-07-11 10:53:57 +1000

    old biopython testing

--- assets/get_references.py
+++ assets/get_references.py
@@ -1,6 +1,5 @@
 import re
 import os
-import logging
 from Bio import Entrez
 import pandas as pd
 from urllib.request import HTTPError
@@ -8,7 +7,7 @@ import time
 
 Entrez.email = "winkie.fong@sydney.edu.au"
 
-viruses="/Users/wfon4473/Documents/Bioinformatics/all_testdirs/meta-gp_reports_tests/db/viruses.txt"
+viruses = os.path.join(os.path.dirname(os.path.dirname(__file__)), "database/virus_nconly.txt")
 fungi = os.path.join(os.path.dirname(os.path.dirname(__file__)), "database/fungi.txt")
 parasites = os.path.join(os.path.dirname(os.path.dirname(__file__)), "database/parasites.txt")
 bacteria="/Users/wfon4473/Documents/Bioinformatics/all_testdirs/meta-gp_reports_tests/db/prokaryotes-refonly.txt"
@@ -20,7 +19,9 @@ def open_virus_file(file):
     db_file["#Organism/Name"].drop_duplicates(inplace= True)
     for index, row in db_file.iterrows():
         db_file.at[index, 'FTP Path'] = get_ftp_path(row["Ref_List"])
-    return db_file[["#Organism/Name", 'FTP Path']]
+    result = db_file[["#Organism/Name", 'FTP Path']]
+    result.to_csv("/Users/wfon4473/Documents/Bioinformatics/all_testdirs/meta-gp_reports_tests/db/virus_ref_list.txt", sep="\t")
+    return result
 
 def open_eukaryotes_file(file):
     db_file = pd.read_csv(file, sep= "\t", header=0)
@@ -43,49 +44,37 @@ def get_ftp_path(terms):
             search_handle = Entrez.esearch(db="assembly", term=term)
             search_record = Entrez.read(search_handle)
             search_handle.close()
+            print(f"searching for {term}")
 
             assembly_id = search_record["IdList"][0]
             assembly_handle = Entrez.esummary(db="assembly", id=assembly_id)
             assembly_summary = Entrez.read(assembly_handle)
             assembly_handle.close()
+            print(f"searching for {assembly_id}")
 
             ftp_path = assembly_summary["DocumentSummarySet"]["DocumentSummary"][0]["FtpPath_RefSeq"]
             ftp_paths.append(ftp_path)
+            print(f"found ftp path {ftp_path}")
         except HTTPError:
             time.sleep(2)
+            print(f"too fast!")
             continue
     return ftp_paths[0]
 
 def get_ftp_path_eukaryotes(term):
     try:
         search_handle = Entrez.esearch(db="assembly", term=term)
-        logging.info(f"Searching for {term}")
         search_record = Entrez.read(search_handle)
         search_handle.close()
 
         assembly_id = search_record["IdList"][0]
-        logging.info(f"Found {term}, getting Assembly ID {assembly_id}")
         assembly_handle = Entrez.esummary(db="assembly", id=assembly_id)
         assembly_summary = Entrez.read(assembly_handle)
         assembly_handle.close()
 
         ftp_path = assembly_summary["DocumentSummarySet"]["DocumentSummary"][0]["FtpPath_RefSeq"]
-        logging.info(f"Found {assembly_id}, ftp path for this sample is {ftp_path}, adding to dataframe")
     except HTTPError:
         time.sleep(2)
-        logging.info("Too fast, slowing down")
     return ftp_path
 
-def combine_ref_lists():
-    virus = open_virus_file(viruses)
-    logging.info(f"Opening {virus}")
-    fungus = open_eukaryotes_file(fungi)
-    logging.info(f"Opening {fungi}")
-    parasite = open_eukaryotes_file(parasites)
-    logging.info(f"Opening {parasites}")
-    bacterium_db = pd.read_csv(bacteria, sep= "\t", header=0)
-    bacterium = bacterium_db[["#Organism/Name", "FTP Path"]]
-    logging.info(f"Opening {bacteria}")
-    combined = pd.concat([virus, fungus, parasite, bacterium])
-    combined.to_csv(os.path.join(os.path.dirname(os.path.dirname(__file__)), "database/ref_list.txt"), sep="\t")
-    logging.info(f"Almost done! joining to csv")
+open_virus_file(viruses)
