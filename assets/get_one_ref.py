755d7367a5a4b086eedab77c7880d8b44371dc7c
LilWinx <lil.winx@gmail.com>
2023-07-13 09:17:47 +1000

    download ref as well

--- assets/get_one_ref.py
+++ assets/get_one_ref.py
@@ -1,17 +1,18 @@
 import re
 import sys
-import os
+import subprocess
 from Bio import Entrez
 import pandas as pd
 from urllib.request import HTTPError
 import time
 
-Entrez.email = ""
-Entrez.api_key = ""
+Entrez.email = "winkie.fong@sydney.edu.au"
+Entrez.api_key = "dfa1ec2df7ac97add34b8fdec4d128a58a09"
 
 query = sys.argv[1]
-db = "/Users/wfon4473/Documents/Bioinformatics/all_testdirs/meta-gp_reports_tests/db/full-ncbi-list.txt"
-#db = "/project/MetaGP/ncbi_db/full-ncbi-list.txt"
+outdir = sys.argv[2]
+#db = "/Users/wfon4473/Documents/Bioinformatics/all_testdirs/meta-gp_reports_tests/db/full-ncbi-list.txt"
+db = "/project/MetaGP/ncbi_db/full-ncbi-list.txt"
 
 def search_query(query):
     ftp_path = None
@@ -28,8 +29,8 @@ def search_query(query):
         eukaryote_match = match[match["Kingdom/Phylum"] == "Eukaryotes"]
         if len(eukaryote_match) > 0:
             ftp_path = get_ftp_path(eukaryote_match["Assembly Accession"])
-        
     print(ftp_path)
+    download_ref(ftp_path, outdir)
 
 def extract_NC(string):
     matches = re.findall(r'NC_\d+\.\d+', string)
@@ -61,5 +62,18 @@ def get_ftp_path(terms):
             print(f"too fast!")
             continue
     return ftp_paths[0]
+
+def download_ref(ftp_path):
+    rsync_path = ftp_path.replace("ftp:", "rsync:")
+    retries = 10
+    for attempt in range(1, retries + 1):
+        try:
+            subprocess.call(["rsync", "--copy-links", "--recursive", "--times", "--verbose", rsync_path, outdir])
+            print(f"Downloading {rsync_path} from NCBI")
+        except subprocess.CalledProcessError as e:
+            print(f"Download attempt {attempt} failed: {e}")
+            print(f"Retrying...")
+            time.sleep(1)
+    print('Maximum number of retries reached. Download failed.')
 
 search_query(query)
