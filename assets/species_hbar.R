ca5195d2c821bbd016be4cb4e45b7bb83d39b93e
LilWinx <lil.winx@gmail.com>
2023-12-11 16:36:55 +1100

    fix hbar dplyr mutate issue

--- assets/species_hbar.R
+++ assets/species_hbar.R
@@ -1,7 +1,8 @@
 list.of.packages <- c(
   "ggplot2",
   "tidyverse",
-  "patchwork"
+  "patchwork",
+  "dplyr"
   )
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
@@ -52,6 +53,10 @@ selected <- subset(selected, phylum != "Chordata" & genus != "Unknown")
 s_total_rpm <- sum(species$rpm_sample) # total excluding Chordata & Unknowns
 sorted <- selected[order(selected$rpm_sample, decreasing = TRUE), ]
 sorted <- subset(sorted, !is.na(species) & species != "")
+sorted <- sorted %>%
+  group_by(species) %>%
+  slice(which.max(zscore)) %>%
+  ungroup()
 top_10 <- head(sorted, 10)
 top_10 <- top_10[, c("species", "rpm_sample")]
 top_10$percentage <- (top_10$rpm_sample / s_total_rpm) * 100
@@ -82,10 +87,10 @@ colours <- c('#0079bf',
              '#bc9b6a',
              '#c4c9cc')
 
-species_freq <- table(new_species$Species)
+species_freq <- table(new_species$species)
 sorted_species <- names(sort(species_freq, decreasing = TRUE))
 sorted_species <- c(sorted_species[sorted_species != "Other"], "Other")
-new_speciessSpecies <- factor(new_species$species, levels = sorted_species)
+new_species$species <- factor(new_species$species, levels = sorted_species)
 
 sorted <- mutate(new_species,
                  species = fct_infreq(new_species$species, w = new_species$percentage) |> fct_other(drop = "Other"))
