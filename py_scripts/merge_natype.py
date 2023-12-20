378970be0cd316bc3dcd7c4bec94768beee33f21
LilWinx <lil.winx@gmail.com>
2023-12-20 22:11:13 +1100

    logging to debug hpc

--- py_scripts/merge_natype.py
+++ py_scripts/merge_natype.py
@@ -1,6 +1,8 @@
 import os
+import logging
 import pandas as pd
 from py_scripts import assists
+
 
 
 
@@ -11,7 +13,9 @@ Script specifically to control the merge of zscore csv from both RNA and DNA
 
 def merge_dna_and_rna(found_list):
     dna_file = next((file for file in found_list if 'CzDna' in file), None)
+    logging.info(f"DNA file: {dna_file}")
     rna_file = next((file for file in found_list if 'CzRna' in file), None)
+    logging.info(f"RNA file: {rna_file}")
     dna_df = assists.load_csv(dna_file)
     rna_df = assists.load_csv(rna_file)
 
